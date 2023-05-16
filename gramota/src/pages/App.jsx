7ee3bcb6a1d1d409557d6cbf4a5803a252920b44
silach93 <rusgamingtactics@mail.ru>
2023-05-16 16:21:57 +0700

    eee,boy

--- gramota/src/pages/App.jsx
+++ gramota/src/pages/App.jsx
@@ -2,17 +2,32 @@ import RoundBox from '../components/ui/roundBox'
 import { text } from './text.data'
 
 function App() {
+  // function next(){
+
+  // }
+
   return (
-    <div className={'container'}>
-      <h1 className={'title'}>Куᴘс: «повышᴇниᴇ интᴇᴘнᴇт-гᴘᴀмотности»</h1>
-      <div className={'card-wrap'}>
-        {
-          text.length ? text.map(text =>
-            <RoundBox key={text.id} text={text} />
-          ) : <p>Ошибка</p>
-        }
+    <div>
+      <div className='mainpage'>
+        <div className='maintitle'>
+          <h1 style={{ textAlign: 'center'}}>доҕᴘо пожᴀловᴀть!</h1>
+          <h1 className={'title'}>Куᴘс: «повышᴇниᴇ интᴇᴘнᴇт-гᴘᴀмотности»</h1>
+        </div>
+        <div className={'container'}>
+          <div className={'card-wrap'}>
+            {
+              text.length ? text.map(text =>
+                <RoundBox key={text.id} text={text} />
+              ) : <p>Ошибка</p>
+            }
+            <div className={'card'}>
+              <h5 className={'card__title'}>Пᴇᴘᴇход к оҕучᴇнию</h5>
+              <button className='mainbtn' onClick="next"></button>
+            </div>
+          </div>
+
+        </div>
       </div>
-      
     </div>
   )
 }
